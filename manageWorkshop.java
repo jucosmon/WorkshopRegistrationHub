4108812e7e6dad17d3b072ee872d1850f6972494
jucosmon <udtohanrj@gmail.com>
2023-11-17 15:07:13 +0800

    wapa nahuman nga design sa manageworkshop kay galibog ko

--- manageWorkshop.java
+++ manageWorkshop.java
@@ -1,11 +1,17 @@
 import javax.swing.*;
-import java.awt.event.*;
 import java.sql.*;
 import java.util.Vector;
 
 import javax.swing.table.*;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 public class manageWorkshop extends JFrame {
+
     JTable table;
     int code;
     String title;
@@ -15,16 +21,82 @@ public class manageWorkshop extends JFrame {
     JButton edit, delete, okay, okay1, okay2, delete_okay;
 
     public manageWorkshop(String email) {
+        // images used
+        ImageIcon logo = new ImageIcon( // setting up image size
+                new ImageIcon("images/logo.png").getImage().getScaledInstance(65, 65, Image.SCALE_DEFAULT));
+        ;
 
-        String[] columnNames = { "Workshop Code", "Title", "Description", "Event Date", "Venue", "Speakers", "Host",
-                "Organization" };
         String[] columnfields = { "title", "description", "event_date", "venue", "speakers", "host", "org" };
         this.setLayout(null);
         this.setSize(600, 600);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        showDataTable(email);
+        JPanel tablePanel = new JPanel();
 
+        // adding table data
+        String[] columnNames = { "Workshop Code", "Title", "Description", "Event Date", "Venue", "Speakers", "Host",
+                "Organization" };
+        DefaultTableModel model = new DefaultTableModel();
+        model.setColumnIdentifiers(columnNames);
+        table = new JTable();
+        table.setModel(model);
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+        table.setFillsViewportHeight(true);
+        JScrollPane scroll = new JScrollPane(table);
+        scroll.setHorizontalScrollBarPolicy(
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scroll.setVerticalScrollBarPolicy(
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/pfbaliwis", "root", "");
+            PreparedStatement pst = con.prepareStatement("select * from workshop where email = ?");
+            pst.setString(1, email);
+            ResultSet rs = pst.executeQuery();
+            int i = 0;
+            int ws_code;
+            String title = "";
+            String description = "";
+            String event_date = "";
+            String venue = "";
+            String speakers = "";
+            String host = "";
+            String org = "";
+            while (rs.next()) {
+                ws_code = rs.getInt("ws_code");
+                title = rs.getString("title");
+                description = rs.getString("description");
+                event_date = rs.getString("event_date");
+                venue = rs.getString("venue");
+                speakers = rs.getString("speakers");
+                host = rs.getString("host");
+                org = rs.getString("org");
+
+                model.addRow(new Object[] { ws_code, title, description, event_date, venue, speakers, host, org });
+                i++;
+            }
+            if (i < 1) {
+                JOptionPane.showMessageDialog(null, "No Record Found", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+            if (i == 1) {
+                System.out.println(i + " Record Found");
+            } else {
+                System.out.println(i + " Records Found");
+            }
+        } catch (Exception e) {
+
+        }
+
+        tablePanel.add(scroll);
+        tablePanel.setVisible(true);
+        tablePanel.setSize(700, 300);
+        tablePanel.setBorder(new EmptyBorder(10, 30, 30, 30)); // add padding or margin
+        tablePanel.setBackground(new Color(68, 93, 72));
+        tablePanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // declaring components
+        l_selectColumn = new JLabel("Choose options: ");
+        l_selectWs = new JLabel("Select Event Code");
         edit = new JButton("Edit");
         delete = new JButton("Delete");
         okay = new JButton("Okay");
@@ -33,9 +105,11 @@ public class manageWorkshop extends JFrame {
         delete_okay = new JButton("Delete");
         selectColumn = new JComboBox(columnfields);
         textField = new JTextField();
-
-        edit.setBounds(0, 0, 100, 30);
-        delete.setBounds(0, 50, 100, 30);
+        JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 100, 0));
+        buttonsPanel.setBorder(new EmptyBorder(10, 30, 30, 30)); // add padding or margin
+        buttonsPanel.setBackground(new Color(68, 93, 72));
+        buttonsPanel.add(edit);
+        buttonsPanel.add(delete);
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -60,29 +134,42 @@ public class manageWorkshop extends JFrame {
 
         }
 
-        edit.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ef) {
+        JPanel selectPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(10, 5, 10, 5); // add gaps
+        c.anchor = GridBagConstraints.WEST; // aligning it to the left side
 
-                selectWs.setBounds(150, 110, 150, 20);
-                okay.setBounds(300, 110, 100, 30);
+        edit.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent ef) {
+                c.gridy = 0;
+                c.gridx = 0;
+                c.gridwidth = 1;
+                c.fill = GridBagConstraints.NONE;
+                selectPanel.add(l_selectWs, c);
+                c.gridx = 1;
+                c.fill = GridBagConstraints.HORIZONTAL;
+                selectPanel.add(selectWs, c);
+                // okay button
+                c.gridx = 2;
+                c.gridwidth = 1;
+                c.fill = GridBagConstraints.NONE;
+                selectPanel.add(okay, c);
+                selectPanel.revalidate();
+                selectPanel.repaint();
 
                 okay.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent ef) {
 
-                        okay.setBounds(0, 0, 0, 0);
-                        selectWs.setBounds(0, 0, 0, 0);
-                        okay1.setBounds(300, 110, 100, 30);
-                        selectColumn.setBounds(90, 110, 100, 30);
+                        selectPanel.add(okay1);
                         int selected = (int) selectWs.getSelectedItem();
 
                         okay1.addActionListener(new ActionListener() {
                             public void actionPerformed(ActionEvent ef) {
 
                                 String column = (String) selectColumn.getSelectedItem();
-                                selectColumn.setBounds(0, 0, 0, 0);
-                                okay1.setBounds(0, 0, 0, 0);
-                                okay2.setBounds(150, 160, 300, 30);
-                                textField.setBounds(150, 110, 300, 30);
+                                selectPanel.add(textField);
+                                selectPanel.add(okay2);
                                 System.out.println(selected + " " + column);
 
                                 okay2.addActionListener(new ActionListener() {
@@ -186,9 +273,8 @@ public class manageWorkshop extends JFrame {
 
         delete.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ef) {
-
-                selectWs.setBounds(150, 110, 150, 20);
-                delete_okay.setBounds(300, 110, 100, 30);
+                selectPanel.add(selectColumn);
+                selectPanel.add(delete_okay);
 
                 delete_okay.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent ef) {
@@ -210,77 +296,22 @@ public class manageWorkshop extends JFrame {
 
             }
         });
+        // frame design ug properties
+        this.setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocation(500, 300);
+        this.setTitle("Role");
+        this.setIconImage(logo.getImage());
+        this.setResizable(false);
+        this.getContentPane().setBackground(new Color(68, 93, 72));
 
-        this.add(selectWs);
-        this.add(delete);
-        this.add(edit);
-        this.add(okay);
-        this.add(selectColumn);
-        this.add(textField);
-        this.add(okay1);
-        this.add(okay2);
-        this.add(delete_okay);
+        // adding components
+        this.add(tablePanel);
+        this.add(buttonsPanel);
+        this.add(selectPanel);
 
-    }
-
-    public void showDataTable(String email) {
-        JFrame frame1 = new JFrame();
-        String[] columnNames = { "Workshop Code", "Title", "Description", "Event Date", "Venue", "Speakers", "Host",
-                "Organization" };
-        DefaultTableModel model = new DefaultTableModel();
-        model.setColumnIdentifiers(columnNames);
-        table = new JTable();
-        table.setModel(model);
-        table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-        table.setFillsViewportHeight(true);
-        JScrollPane scroll = new JScrollPane(table);
-        scroll.setHorizontalScrollBarPolicy(
-                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scroll.setVerticalScrollBarPolicy(
-                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/pfbaliwis", "root", "");
-            PreparedStatement pst = con.prepareStatement("select * from workshop where email = ?");
-            pst.setString(1, email);
-            ResultSet rs = pst.executeQuery();
-            int i = 0;
-            int ws_code;
-            String title = "";
-            String description = "";
-            String event_date = "";
-            String venue = "";
-            String speakers = "";
-            String host = "";
-            String org = "";
-            while (rs.next()) {
-                ws_code = rs.getInt("ws_code");
-                title = rs.getString("title");
-                description = rs.getString("description");
-                event_date = rs.getString("event_date");
-                venue = rs.getString("venue");
-                speakers = rs.getString("speakers");
-                host = rs.getString("host");
-                org = rs.getString("org");
-
-                model.addRow(new Object[] { ws_code, title, description, event_date, venue, speakers, host, org });
-                i++;
-            }
-            if (i < 1) {
-                JOptionPane.showMessageDialog(null, "No Record Found", "Error", JOptionPane.ERROR_MESSAGE);
-            }
-            if (i == 1) {
-                System.out.println(i + " Record Found");
-            } else {
-                System.out.println(i + " Records Found");
-            }
-        } catch (Exception e) {
-
-        }
-
-        frame1.add(scroll);
-        frame1.setVisible(true);
-        frame1.setSize(400, 300);
+        this.pack();
+        this.setVisible(true);
 
     }
 
